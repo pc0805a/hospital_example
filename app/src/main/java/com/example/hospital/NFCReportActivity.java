67d96714e9ed25a64c15023ad71ab284d7803abd
張家瑋 <pc0805a@gmail.com>
2015-10-13 09:42:54 +0800

    connect db with AsyncTask

--- app/src/main/java/com/example/hospital/NFCReportActivity.java
+++ app/src/main/java/com/example/hospital/NFCReportActivity.java
@@ -3,6 +3,7 @@ package com.example.hospital;
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -11,8 +12,21 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class NFCReportActivity extends Activity {
@@ -73,8 +87,9 @@ public class NFCReportActivity extends Activity {
         getMemberInfo();
         if(hasLocalData)
         {
-           // new FetchDBInfo(mid, imei).getInfo();
+
         }
+       new fetchData().execute();
     }
 
     @Override
@@ -143,8 +158,88 @@ public class NFCReportActivity extends Activity {
         }
     }
 
-    private void getMemberDayList()
+
+    String postURL = "http://140.136.150.92/fetchDBInfo.php";
+
+    private class fetchData extends AsyncTask<String, Void, String>
     {
 
+        @Override
+        protected String doInBackground(String... params) {
+
+            HashMap<String, String> postDataParams = new HashMap<String, String>();
+
+            postDataParams.put("id", "a123456789");
+            postDataParams.put("imei", "123");
+
+
+            String result = getInfo(postURL, postDataParams);
+            Log.v(TAG, "result: "+result + "\nend");
+            return null;
+        }
+
+
+    }
+
+    private String getInfo(String requestURL,
+                           HashMap<String, String> postDataParams)
+    {
+        URL url;//140.136.150.92/fetchDBInfo.php
+        String response = "";
+        try {
+            url = new URL(requestURL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode=conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line=br.readLine()) != null) {
+                    response+=line;
+                }
+            }
+            else {
+                response="";
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
     }
 }
+
