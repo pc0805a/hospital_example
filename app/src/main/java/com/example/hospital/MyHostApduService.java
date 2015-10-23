e46a0b5a3519ff122fe42fef5dd1eae0a9e28f7a
張家瑋 <pc0805a@gmail.com>
2015-10-23 11:36:00 +0800

    set nfc send data

--- app/src/main/java/com/example/hospital/MyHostApduService.java
+++ app/src/main/java/com/example/hospital/MyHostApduService.java
@@ -6,14 +6,25 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MyHostApduService extends HostApduService {
+
+    private static final String TAG = MyHostApduService.class.getSimpleName();
 	
 	private MemberInfo meberInfo;
 	private MemberDayList memberDayList;
 	private String memberID;
 
+	private String division;
+	private String doctor;
+	private String year	;
+	private String month;
+	private String day;
+	private String time;
+	private int num;
+
+
 	private void getMemberInfo() {
 
-		meberInfo = new  MemberInfo(this);
+		meberInfo = new MemberInfo(this);
 		meberInfo.open();
 
 		Cursor cursor = meberInfo.getAll();
@@ -23,10 +34,32 @@ public class MyHostApduService extends HostApduService {
 			memberID = cursor.getString(1);
 		}
 	}
+	private void getSelectedDayList()
+	{
+		memberDayList = new MemberDayList(this);
+		memberDayList.open();
+
+		Cursor cursor = memberDayList.getSpinnerSelected();
+		int row_num = cursor.getCount();
+		if (row_num != 0) {
+			cursor.moveToFirst();
+			division = cursor.getString(1);
+			doctor = cursor.getString(2);
+			year = cursor.getString(3);
+			month = cursor.getString(4);
+			day = cursor.getString(5);
+			time = cursor.getString(6);
+			num= cursor.getInt(7);
+		}
+
+        Log.v(TAG, doctor);
+
+	}
 
 	@Override
 	public byte[] processCommandApdu(byte[] apdu, Bundle extras) {
 		getMemberInfo();
+        getSelectedDayList();
 		if (selectAidApdu(apdu)) {
 			Log.i("HCEDEMO", "Application selected");
 			return getWelcomeMessage();
@@ -42,8 +75,15 @@ public class MyHostApduService extends HostApduService {
 	}
 
 	private byte[] getNextMessage() {
-		return ("Message from android: " + memberID).getBytes();
-	}
+		return ("Message from android:" + memberID + ":" +
+                                            division + ":" +
+                                            doctor + ":" +
+                                            year + ":" +
+                                            month + ":" +
+                                            day + ":" +
+                                            time + ":" +
+                                            num).getBytes();
+    }
 
 	private boolean selectAidApdu(byte[] apdu) {
 		return apdu.length >= 2 && apdu[0] == (byte)0 && apdu[1] == (byte)0xa4;
